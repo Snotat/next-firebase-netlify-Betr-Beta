d3875d1b971d2fc7098ec2f6f6854acebab39cae
Shuaib Olawale <snotatonline@gmail.com>
2023-11-18 01:32:11 +0100

    subscription-testimonial-emailcard

---
+++ app/components/subscription/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function subscription() {
+  return <div></div>;
+}
+
+export default subscription;
---
+++ app/testimonials/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function testimonials() {
+  return <div className="ft-25 bold">Testimonials</div>;
+}
+
+export default testimonials;
